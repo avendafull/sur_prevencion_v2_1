bcdb9699040abccadbda2ea922cc45c564a1c53d
Daniel Avendaño <74194591+avendafull@users.noreply.github.com>
2025-05-30 12:59:52 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-cambia-esto'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///surpensionistas.sqlite'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
